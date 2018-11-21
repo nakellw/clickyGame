e3e669d726e040bb1feb3448d86c35609ce229df
Nakell Williams <nakellw@yahoo.com>
2018-11-21 17:40:48 -0500

    Updates

---
+++ precache-manifest.c6e1c4da7af5df70e82bfde874b08caa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ef1818bcebd05a4bfe7d",
+    "url": "/static/js/main.ef1818bc.chunk.js"
+  },
+  {
+    "revision": "8fdf2345b7a4e11badcc",
+    "url": "/static/js/1.8fdf2345.chunk.js"
+  },
+  {
+    "revision": "ef1818bcebd05a4bfe7d",
+    "url": "/static/css/main.8b40820c.chunk.css"
+  },
+  {
+    "revision": "6119978e186d89542cf21c42ffae1313",
+    "url": "/index.html"
+  }
+];
